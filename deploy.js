cfb0fcfd11fb1e3092c18de9309c453ab4f4fd9c
huasmc <huasmc@gmail.com>
2023-01-03 18:10:02 -0400

    feat: simple storage smart contract

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+	// Ganache local RPC url http://127.0.0.1:7545
+	const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+	const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+	const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+	const binary = fs.readFileSync(
+		"./SimpleStorage_sol_SimpleStorage.bin",
+		"utf8"
+	);
+	const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+	console.log("Deploying, please wait...");
+	const contract = await contractFactory.deploy();
+	await contract.deployTransaction.wait(1);
+	console.log(`Contract Address: ${contract.address}`);
+	const currentFavoriteNumber = await contract.retrieve();
+	console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+	const transactionResponse = await contract.store("7");
+	const transactionReceipt = await transactionResponse.wait(1);
+	const updatedFavoriteNumber = await contract.retrieve();
+	console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
+}
+
+main();
